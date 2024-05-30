8f190840801d9100f5d61fa3363c282754d8b9a1
zzc6332 <63202477+zzc6332@users.noreply.github.com>
2024-05-31 04:50:26 +0800

    chore: v0.1.7

--- webpack.config.js
+++ webpack.config.js
@@ -40,7 +40,7 @@ module.exports = () => {
             to: "package.json",
           },
           { from: "README.md", to: "README.md" },
-          { from: "LICENSE", to: "LICENSE" },
+          { from: "LICENSE", to: "LICENSE", toType: "file" },
         ],
       })
     );
